2ec5872eb5e625b661e510e1352dd88bee6f2e52
cagatay artok <cartok01@gmail.com>
2024-04-05 13:52:50 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-262d58a7-7366-4ea2-85ff-c396f38881f9/"
 
 OUTPUT_DIR="logs"
 
